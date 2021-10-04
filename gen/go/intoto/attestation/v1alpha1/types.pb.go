aec2a6dc5a1c62e71b1d96521259ce2b30de5cc3
Alex Ashley <alexa@liatrio.com>
2021-10-04 16:04:41 -0400

    Regenerate

--- gen/go/intoto/attestation/v1alpha1/types.pb.go
+++ gen/go/intoto/attestation/v1alpha1/types.pb.go
@@ -16,9 +16,9 @@
 // versions:
 // 	protoc-gen-go v1.25.0
 // 	protoc        v3.18.0
-// source: intoto/attestation/types.proto
+// source: intoto/attestation/v1alpha1/types.proto
 
-package v1alpha2
+package v2alpha1
 
 import (
 	proto "github.com/golang/protobuf/proto"
@@ -53,7 +53,7 @@ type TypeURI struct {
 func (x *TypeURI) Reset() {
 	*x = TypeURI{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_intoto_attestation_types_proto_msgTypes[0]
+		mi := &file_intoto_attestation_v1alpha1_types_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -66,7 +66,7 @@ func (x *TypeURI) String() string {
 func (*TypeURI) ProtoMessage() {}
 
 func (x *TypeURI) ProtoReflect() protoreflect.Message {
-	mi := &file_intoto_attestation_types_proto_msgTypes[0]
+	mi := &file_intoto_attestation_v1alpha1_types_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -79,7 +79,7 @@ func (x *TypeURI) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use TypeURI.ProtoReflect.Descriptor instead.
 func (*TypeURI) Descriptor() ([]byte, []int) {
-	return file_intoto_attestation_types_proto_rawDescGZIP(), []int{0}
+	return file_intoto_attestation_v1alpha1_types_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *TypeURI) GetValue() string {
@@ -104,7 +104,7 @@ type ResourceURI struct {
 func (x *ResourceURI) Reset() {
 	*x = ResourceURI{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_intoto_attestation_types_proto_msgTypes[1]
+		mi := &file_intoto_attestation_v1alpha1_types_proto_msgTypes[1]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -117,7 +117,7 @@ func (x *ResourceURI) String() string {
 func (*ResourceURI) ProtoMessage() {}
 
 func (x *ResourceURI) ProtoReflect() protoreflect.Message {
-	mi := &file_intoto_attestation_types_proto_msgTypes[1]
+	mi := &file_intoto_attestation_v1alpha1_types_proto_msgTypes[1]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -130,7 +130,7 @@ func (x *ResourceURI) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use ResourceURI.ProtoReflect.Descriptor instead.
 func (*ResourceURI) Descriptor() ([]byte, []int) {
-	return file_intoto_attestation_types_proto_rawDescGZIP(), []int{1}
+	return file_intoto_attestation_v1alpha1_types_proto_rawDescGZIP(), []int{1}
 }
 
 func (x *ResourceURI) GetValue() string {
@@ -154,7 +154,7 @@ type DigestSet struct {
 func (x *DigestSet) Reset() {
 	*x = DigestSet{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_intoto_attestation_types_proto_msgTypes[2]
+		mi := &file_intoto_attestation_v1alpha1_types_proto_msgTypes[2]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -167,7 +167,7 @@ func (x *DigestSet) String() string {
 func (*DigestSet) ProtoMessage() {}
 
 func (x *DigestSet) ProtoReflect() protoreflect.Message {
-	mi := &file_intoto_attestation_types_proto_msgTypes[2]
+	mi := &file_intoto_attestation_v1alpha1_types_proto_msgTypes[2]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -180,7 +180,7 @@ func (x *DigestSet) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use DigestSet.ProtoReflect.Descriptor instead.
 func (*DigestSet) Descriptor() ([]byte, []int) {
-	return file_intoto_attestation_types_proto_rawDescGZIP(), []int{2}
+	return file_intoto_attestation_v1alpha1_types_proto_rawDescGZIP(), []int{2}
 }
 
 func (x *DigestSet) GetSha256() string {
@@ -190,44 +190,45 @@ func (x *DigestSet) GetSha256() string {
 	return ""
 }
 
-var File_intoto_attestation_types_proto protoreflect.FileDescriptor
+var File_intoto_attestation_v1alpha1_types_proto protoreflect.FileDescriptor
 
-var file_intoto_attestation_types_proto_rawDesc = []byte{
-	0x0a, 0x1e, 0x69, 0x6e, 0x74, 0x6f, 0x74, 0x6f, 0x2f, 0x61, 0x74, 0x74, 0x65, 0x73, 0x74, 0x61,
-	0x74, 0x69, 0x6f, 0x6e, 0x2f, 0x74, 0x79, 0x70, 0x65, 0x73, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f,
-	0x12, 0x12, 0x69, 0x6e, 0x74, 0x6f, 0x74, 0x6f, 0x2e, 0x61, 0x74, 0x74, 0x65, 0x73, 0x74, 0x61,
-	0x74, 0x69, 0x6f, 0x6e, 0x22, 0x1f, 0x0a, 0x07, 0x54, 0x79, 0x70, 0x65, 0x55, 0x52, 0x49, 0x12,
-	0x14, 0x0a, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05,
-	0x76, 0x61, 0x6c, 0x75, 0x65, 0x22, 0x23, 0x0a, 0x0b, 0x52, 0x65, 0x73, 0x6f, 0x75, 0x72, 0x63,
-	0x65, 0x55, 0x52, 0x49, 0x12, 0x14, 0x0a, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x18, 0x01, 0x20,
-	0x01, 0x28, 0x09, 0x52, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x22, 0x29, 0x0a, 0x09, 0x44, 0x69,
-	0x67, 0x65, 0x73, 0x74, 0x53, 0x65, 0x74, 0x12, 0x16, 0x0a, 0x06, 0x73, 0x68, 0x61, 0x32, 0x35,
-	0x36, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x06, 0x73, 0x68, 0x61, 0x32, 0x35, 0x36, 0x4a,
-	0x04, 0x08, 0x02, 0x10, 0x33, 0x42, 0x25, 0x5a, 0x23, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e,
-	0x63, 0x6f, 0x6d, 0x2f, 0x72, 0x6f, 0x64, 0x65, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x72,
-	0x6f, 0x64, 0x65, 0x2f, 0x76, 0x31, 0x61, 0x6c, 0x70, 0x68, 0x61, 0x32, 0x62, 0x06, 0x70, 0x72,
-	0x6f, 0x74, 0x6f, 0x33,
+var file_intoto_attestation_v1alpha1_types_proto_rawDesc = []byte{
+	0x0a, 0x27, 0x69, 0x6e, 0x74, 0x6f, 0x74, 0x6f, 0x2f, 0x61, 0x74, 0x74, 0x65, 0x73, 0x74, 0x61,
+	0x74, 0x69, 0x6f, 0x6e, 0x2f, 0x76, 0x31, 0x61, 0x6c, 0x70, 0x68, 0x61, 0x31, 0x2f, 0x74, 0x79,
+	0x70, 0x65, 0x73, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x1b, 0x69, 0x6e, 0x74, 0x6f, 0x74,
+	0x6f, 0x2e, 0x61, 0x74, 0x74, 0x65, 0x73, 0x74, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x2e, 0x76, 0x31,
+	0x61, 0x6c, 0x70, 0x68, 0x61, 0x31, 0x22, 0x1f, 0x0a, 0x07, 0x54, 0x79, 0x70, 0x65, 0x55, 0x52,
+	0x49, 0x12, 0x14, 0x0a, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09,
+	0x52, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x22, 0x23, 0x0a, 0x0b, 0x52, 0x65, 0x73, 0x6f, 0x75,
+	0x72, 0x63, 0x65, 0x55, 0x52, 0x49, 0x12, 0x14, 0x0a, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x18,
+	0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x22, 0x29, 0x0a, 0x09,
+	0x44, 0x69, 0x67, 0x65, 0x73, 0x74, 0x53, 0x65, 0x74, 0x12, 0x16, 0x0a, 0x06, 0x73, 0x68, 0x61,
+	0x32, 0x35, 0x36, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x06, 0x73, 0x68, 0x61, 0x32, 0x35,
+	0x36, 0x4a, 0x04, 0x08, 0x02, 0x10, 0x33, 0x42, 0x25, 0x5a, 0x23, 0x67, 0x69, 0x74, 0x68, 0x75,
+	0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x72, 0x6f, 0x64, 0x65, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f,
+	0x2f, 0x72, 0x6f, 0x64, 0x65, 0x2f, 0x76, 0x32, 0x61, 0x6c, 0x70, 0x68, 0x61, 0x31, 0x62, 0x06,
+	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
-	file_intoto_attestation_types_proto_rawDescOnce sync.Once
-	file_intoto_attestation_types_proto_rawDescData = file_intoto_attestation_types_proto_rawDesc
+	file_intoto_attestation_v1alpha1_types_proto_rawDescOnce sync.Once
+	file_intoto_attestation_v1alpha1_types_proto_rawDescData = file_intoto_attestation_v1alpha1_types_proto_rawDesc
 )
 
-func file_intoto_attestation_types_proto_rawDescGZIP() []byte {
-	file_intoto_attestation_types_proto_rawDescOnce.Do(func() {
-		file_intoto_attestation_types_proto_rawDescData = protoimpl.X.CompressGZIP(file_intoto_attestation_types_proto_rawDescData)
+func file_intoto_attestation_v1alpha1_types_proto_rawDescGZIP() []byte {
+	file_intoto_attestation_v1alpha1_types_proto_rawDescOnce.Do(func() {
+		file_intoto_attestation_v1alpha1_types_proto_rawDescData = protoimpl.X.CompressGZIP(file_intoto_attestation_v1alpha1_types_proto_rawDescData)
 	})
-	return file_intoto_attestation_types_proto_rawDescData
+	return file_intoto_attestation_v1alpha1_types_proto_rawDescData
 }
 
-var file_intoto_attestation_types_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
-var file_intoto_attestation_types_proto_goTypes = []interface{}{
-	(*TypeURI)(nil),     // 0: intoto.attestation.TypeURI
-	(*ResourceURI)(nil), // 1: intoto.attestation.ResourceURI
-	(*DigestSet)(nil),   // 2: intoto.attestation.DigestSet
+var file_intoto_attestation_v1alpha1_types_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
+var file_intoto_attestation_v1alpha1_types_proto_goTypes = []interface{}{
+	(*TypeURI)(nil),     // 0: intoto.attestation.v1alpha1.TypeURI
+	(*ResourceURI)(nil), // 1: intoto.attestation.v1alpha1.ResourceURI
+	(*DigestSet)(nil),   // 2: intoto.attestation.v1alpha1.DigestSet
 }
-var file_intoto_attestation_types_proto_depIdxs = []int32{
+var file_intoto_attestation_v1alpha1_types_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
 	0, // [0:0] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -235,13 +236,13 @@ var file_intoto_attestation_types_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_intoto_attestation_types_proto_init() }
-func file_intoto_attestation_types_proto_init() {
-	if File_intoto_attestation_types_proto != nil {
+func init() { file_intoto_attestation_v1alpha1_types_proto_init() }
+func file_intoto_attestation_v1alpha1_types_proto_init() {
+	if File_intoto_attestation_v1alpha1_types_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_intoto_attestation_types_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_intoto_attestation_v1alpha1_types_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*TypeURI); i {
 			case 0:
 				return &v.state
@@ -253,7 +254,7 @@ func file_intoto_attestation_types_proto_init() {
 				return nil
 			}
 		}
-		file_intoto_attestation_types_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+		file_intoto_attestation_v1alpha1_types_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*ResourceURI); i {
 			case 0:
 				return &v.state
@@ -265,7 +266,7 @@ func file_intoto_attestation_types_proto_init() {
 				return nil
 			}
 		}
-		file_intoto_attestation_types_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+		file_intoto_attestation_v1alpha1_types_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*DigestSet); i {
 			case 0:
 				return &v.state
@@ -282,18 +283,18 @@ func file_intoto_attestation_types_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_intoto_attestation_types_proto_rawDesc,
+			RawDescriptor: file_intoto_attestation_v1alpha1_types_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   3,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
-		GoTypes:           file_intoto_attestation_types_proto_goTypes,
-		DependencyIndexes: file_intoto_attestation_types_proto_depIdxs,
-		MessageInfos:      file_intoto_attestation_types_proto_msgTypes,
+		GoTypes:           file_intoto_attestation_v1alpha1_types_proto_goTypes,
+		DependencyIndexes: file_intoto_attestation_v1alpha1_types_proto_depIdxs,
+		MessageInfos:      file_intoto_attestation_v1alpha1_types_proto_msgTypes,
 	}.Build()
-	File_intoto_attestation_types_proto = out.File
-	file_intoto_attestation_types_proto_rawDesc = nil
-	file_intoto_attestation_types_proto_goTypes = nil
-	file_intoto_attestation_types_proto_depIdxs = nil
+	File_intoto_attestation_v1alpha1_types_proto = out.File
+	file_intoto_attestation_v1alpha1_types_proto_rawDesc = nil
+	file_intoto_attestation_v1alpha1_types_proto_goTypes = nil
+	file_intoto_attestation_v1alpha1_types_proto_depIdxs = nil
 }
